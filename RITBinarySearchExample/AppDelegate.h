48bf8c5682fffdb81149e5b3d8e106b49418c6c3
Alexander Pronin <pronin.alx@gmail.com>
2015-08-04 14:58:53 +0300

    Initial Commit

---
+++ RITBinarySearchExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RITBinarySearchExample
+//
+//  Created by Aleksandr Pronin on 8/4/15.
+//  Copyright (c) 2015 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RITBinarySearchExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RITBinarySearchExample
+//
+//  Created by Aleksandr Pronin on 8/4/15.
+//  Copyright (c) 2015 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
